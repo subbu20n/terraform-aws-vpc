879bb9549e3d4ee8411436c059fe673466c6702c
subbu <subbunandyala48@gmail.com>
2025-06-25 16:04:47 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id  
 }
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "databse_subnet_ids" {
+  value = aws_subnet.databse[*].id
+}
