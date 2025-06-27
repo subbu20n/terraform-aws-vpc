a9648c8f5defd001647ba16103ecb670bdae0e5a
subbu <subbunandyala48@gmail.com>
2025-06-27 21:46:32 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -1,9 +1,8 @@
 locals {
     common_tags = {
-        project = var.project 
-        environment = var.environment 
-        Terraform = true 
+    
+        Project = var.project 
+        Environment = var.environment 
+        Terraform = "true"
     }
-
-    az_names = slice(data.aws_availability_zones.available.names, 0, 2)
 }
