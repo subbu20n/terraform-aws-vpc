36e40c21e8fa7860fd564030d16aa6c415a6a02d
subbu <subbunandyala48@gmail.com>
2025-06-28 09:25:14 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -56,3 +56,17 @@ variable "nat_gateway_tags" {
     default = {}
 }
 
+variable "public_route_table_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map(string)
+    default  = {}
+}
+
+variable "database_route_table_tags" {
+    type = map(string)
+    default = {}
+}
--- vpc.tf
+++ vpc.tf
@@ -95,6 +95,80 @@ resource "aws_nat_gateway" "main" {
         local.common_tags,
         {
             Name = "${var.project}-${var.environment}"
+        } 
+    )
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id 
+
+    tags = merge(
+        var.public_route_table_tags,
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-public"
         }
     )
 }
+
+resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.main.id 
+
+    tags = merge(
+        var.private_route_table_tags,
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-private"
+        }
+    )
+}
+
+resource "aws_route_table" "database" {
+    vpc_id = aws_vpc.main.id 
+
+
+    tags = merge(
+        var.database_route_table_tags,
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-database"
+        }
+    )
+}
+
+resource "aws_route" "public" {
+    route_table_id = aws_route_table.public.id 
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id 
+}
+
+resource "aws_route" "private" {
+    route_table_id = aws_route_table.private.id 
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id   
+}
+
+resource "aws_route" "database" {
+    route_table_id = aws_route_table.database.id 
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id 
+}
+
+resource "aws_route_table_association" "public" {
+    count = length(var.public_subnet_cidrs)
+    subnet_id = aws_subnet.public[count.index].id 
+    route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+    count = length(var.private_subnet_cidrs)
+    subnet_id = aws_subnet.private[count.index].id
+    route_table_id = aws_route_table.private.id 
+}
+
+resource "aws_route_table_association" "database" {
+    count = length(var.database_subnet_cidrs) 
+    subnet_id = aws_subnet.database[count.index].id
+    route_table_id = aws_route_table.database.id 
+}
+
