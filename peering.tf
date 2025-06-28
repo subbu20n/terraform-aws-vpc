9959d8b56b30a20d0c8e977849397c76806f26da
subbu <subbunandyala48@gmail.com>
2025-06-28 12:01:48 +0530

    terraform

--- peering.tf
+++ peering.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "default" {
     count  = var.is_peering_required? 1 : 0 
 
-    peer_vpc_id = data.aws_vpc.deafult.id 
+    peer_vpc_id = data.aws_vpc.default.id 
     vpc_id = aws_vpc.main.id 
 
   accepter {
