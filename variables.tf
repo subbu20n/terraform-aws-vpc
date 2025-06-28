f44a55831b1be5729b0038e7a1fea7d8468da9a1
subbu <subbunandyala48@gmail.com>
2025-06-28 08:48:06 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -46,3 +46,13 @@ variable "database_subnet_tags" {
     type = map(string) 
     default = {}
 }
+variable "eip_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    type = map(string)
+    default = {}
+}
+
