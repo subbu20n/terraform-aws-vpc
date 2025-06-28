957d71188da647411f84a0b5e580c9e5585486e1
subbu <subbunandyala48@gmail.com>
2025-06-28 09:51:10 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -156,7 +156,7 @@ resource "aws_route" "database" {
 
 resource "aws_route_table_association" "public" {
     count = length(var.public_subnet_cidrs)
-    subnet_id = aws_subnet.public[count.index].index
+    subnet_id = aws_subnet.public[count.index].id
     route_table_id = aws_route_table.public.id 
 }
 
