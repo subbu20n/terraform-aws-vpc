4d214af43f476773bc9a55ab13adcfa660ba3adc
subbu <subbunandyala48@gmail.com>
2025-06-28 11:35:15 +0530

    terraform

--- data.tf
+++ data.tf
@@ -2,6 +2,10 @@ data "aws_availability_zones" "available" {
     state = "available"
 }
 
+data "aws_vpc" "default" {
+    default = true 
+}
+
 #output "azs_info" {
 #    value = data.aws_availability_zones.available 
 #}
