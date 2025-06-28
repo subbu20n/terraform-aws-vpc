6470f93090792fccac97aba9a851abd6dd25d5e7
subbu <subbunandyala48@gmail.com>
2025-06-28 12:22:07 +0530

    terraform

--- peering.tf
+++ peering.tf
@@ -35,7 +35,7 @@ resource "aws_route" "private_peering" {
     count = var.is_peering_required? 1 : 0
     route_table_id = aws_route_table.private.id 
     destination_cidr_block = data.aws_vpc.default.cidr_block 
-    vpc_peering_connection_id = aws_vpc_peeing_connection.default[count.index].id 
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id 
 }
 
 resource "aws_route" "database_peering" {
