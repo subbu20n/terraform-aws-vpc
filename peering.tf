635b7e397b57ce7821ebf7c98dc2ef7b6abfe974
subbu <subbunandyala48@gmail.com>
2025-06-28 12:19:53 +0530

    terraform

--- peering.tf
+++ peering.tf
@@ -2,7 +2,14 @@ resource "aws_vpc_peering_connectin" "default" {
     count = var.is_peering_required? 1 : 0 
     peer_vpc_id = data.aws_vpc.default.id 
     vpc_id = aws_vpc.main.id 
+    
+    accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
 
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
 
 
 
