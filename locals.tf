5c45ff7846f1c99f4ba0a95e61311b9dfdfeea70
subbu <subbunandyala48@gmail.com>
2025-07-05 16:36:50 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -1,9 +1,8 @@
-locals {
+locals { 
   common_tags = {
-    Project = var.project
-    Environment = var.environment
-    Terraform = "true"
+    Project = var.project 
+    Environment = var.environment 
+    Terraform = "true" 
   }
-
   az_names = slice(data.aws_availability_zones.available.names, 0, 2)
-}
+} 
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,14 @@
 output "vpc_id" {
-  value       = aws_vpc.main.id
+    value = aws_vpc.main.id 
 }
-
 output "public_subnet_ids" {
     value = aws_subnet.public[*].id 
 }
 
-output "private_subnet_ids" {
+output "private_subnet_id" {
     value = aws_subnet.private[*].id 
 }
 
-output "database_subnet_ids" {
+output "database_subnet_id" {
     value = aws_subnet.database[*].id 
 }
