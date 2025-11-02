09329ce7264211f43a4700f0782b65c46c7c7531
subbu <subbunandyala48@gmail.com>
2025-11-02 15:16:47 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "aws_vpc" {
-  value = aws_vpc.main.id 
+output "vpc_id" {
+  value       = aws_vpc.main.id
 }
 
-output "aws_subnet_public" {
-  value = aws_subnet.public[*].id 
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
 }
 
-output "aws_subnet_private" {
-  value = aws_subnet.private[*].id 
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
 }
 
-output "aws_subnet_database" {
-  value = aws_subnet.database[*].id 
-}  
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
