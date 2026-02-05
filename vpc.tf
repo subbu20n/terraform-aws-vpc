527af5dd6238d7a8d7abebe5008c1d045e0dac19
subbu <subbunandyala48@gmail.com>
2026-02-05 12:06:35 +0530

    sg

--- vpc.tf
+++ vpc.tf
@@ -157,7 +157,7 @@ resource "aws_route" "database" {
   nat_gateway_id = aws_nat_gateway.main.id  
 }
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public" { 
   count = length(var.public_subnet_cidrs) 
   subnet_id = aws_subnet.public[count.index].id 
   route_table_id = aws_route_table.public.id  
