f6730483e754e62becc4ee04f0bd3f10fd82fbde
subbu <subbunandyala48@gmail.com>
2026-06-17 22:35:23 +0530

    terraform-aws-vpc

--- vpc.tf
+++ vpc.tf
@@ -153,9 +153,9 @@ resource "aws_route" "public" {
 resource "aws_route" "private" {
   route_table_id = aws_route_table.private.id 
   destination_cidr_block = "0.0.0.0/0" 
-  nat_gateway_id = aws_nat_gateway.main.id  
+  nat_gateway_id = aws_nat_gateway.main.id   
 }
-
+       
 resource "aws_route" "database" {
   route_table_id = aws_route_table.database.id 
   destination_cidr_block  = "0.0.0.0/0" 
